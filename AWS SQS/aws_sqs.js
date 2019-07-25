210d82d7b65a541036bce4f4068d280dc4dba68a
mahiya <hiiyan0402@gmail.com>
2019-07-25 17:50:47 +0900

    Add sample code of AWS SQS in Nodejs

---
+++ AWS SQS/aws_sqs.js
@@ -0,0 +1,40 @@
+(async () => {
+
+    // npm install aws-sdk
+    const AWS = require("aws-sdk");
+    AWS.config.update({region: "ap-northeast-1"});
+
+    // AWS SQS クライアントを作成し、URLを指定する
+    const sqs = new AWS.SQS();
+    const queueUrl = "https://sqs.ap-northeast-1.amazonaws.com/xxxxxxx/xxxxxxxx";
+
+    //  メッセージをキューに送信する
+    var sendResult = await sqs.sendMessage({
+        QueueUrl: queueUrl,
+        MessageBody: "Hello AWS SQS !!",
+    }).promise();
+    console.log("Sent Result = " + JSON.stringify(sendResult, null, 2));    
+
+    // メッセージをキューから取得する
+    var received = await sqs.receiveMessage({
+        QueueUrl: queueUrl,
+        MaxNumberOfMessages: 10,
+    }).promise();
+    console.log("Received Data = " + JSON.stringify(received, null, 2));
+
+    // 取得したメッセージ毎に処理をする
+    for(var i in received.Messages) {
+        var message = received.Messages[i];
+        console.log("Message Body = " + message.Body);
+
+        // メッセージをキューから削除する
+        var deleteResult = await sqs.deleteMessage({
+            QueueUrl: queueUrl,
+            ReceiptHandle: message.ReceiptHandle
+        }).promise();
+        console.log("Deleted Sesult = " + JSON.stringify(deleteResult, null, 2));
+    }
+
+})();
+
+
