b3acd245afef8ff9a7f6792091beda1935e039f3
mahiya <hiiyan0402@gmail.com>
2019-08-01 15:44:15 +0900

    modify sample code of getting suggests from AWS CloudSearch to make code easy to read

--- AWS/aws_cloudsearch_suggest.js
+++ AWS/aws_cloudsearch_suggest.js
@@ -1,17 +1,23 @@
-// npm install request request-promise
-var request = require('request-promise');
-var cloudSearchEndpoint = "";
-var suggesterName = "";
-
 (async () => {
 
+    // npm install request request-promise
+    var request = require('request-promise');
+
+    // AWS CloudSearch のエンドポイントと、使用するサジェスト名を指定する
+    var cloudSearchEndpoint = "";
+    var suggesterName = "";
+
+    // 検索クエリを指定する
     var query = "";
+
+    // エンドポイントを呼び出してサジェストを取得する
     var url = cloudSearchEndpoint + "/2013-01-01/suggest?suggester=" + suggesterName + "&q=" + query;
     var options = {
         url: encodeURI(url),
     };
     var resp = await request(options);
 
+    // 取得したサジェストを画面に表示する
     var suggestions = [];
     JSON.parse(resp).suggest.suggestions.forEach((suggestion) => {
         suggestions.push(suggestion.suggestion);
