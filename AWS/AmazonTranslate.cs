5c3851ce51ee406dc7402c3dd3ef7ef0cc25399d
mahiya <hiiyan0402@gmail.com>
2020-01-28 17:44:12 +0900

    add sample code of Amazon Translate (C#)

---
+++ AWS/AmazonTranslate.cs
@@ -0,0 +1,27 @@
+﻿// dotnet add package AWSSDK.Translate --version 3.3.101.7
+using System;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.Translate;
+using Amazon.Translate.Model;
+
+namespace TranslateSample
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var region = RegionEndpoint.APNortheast1;
+            var client = new AmazonTranslateClient(region);
+            var request = new TranslateTextRequest
+            {
+                SourceLanguageCode = "ja",
+                TargetLanguageCode = "en",
+                Text = "おはようございます"
+            };
+            var resp = await client.TranslateTextAsync(request);
+            var result = resp.TranslatedText;
+            Console.WriteLine(result);
+        }
+    }
+}
